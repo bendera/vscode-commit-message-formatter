f5723d512448dd644cf656b4d9e0b9535320598a
bendera <szoftverhiba@gmail.com>
2023-05-01 14:38:45 +0200

    Add configurable settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,61 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import CommitMessageFormatter from "@bendera/commit-message-formatter";
+import CommitMessageFormatter, {
+  CommitMessageFormatterOptions,
+  SubjectFormattingMode,
+} from "@bendera/commit-message-formatter";
+
+function getConfiguration(): CommitMessageFormatterOptions {
+  const globalEditorSettings = vscode.workspace.getConfiguration("editor");
+  const globalTabsize = globalEditorSettings.get("tabSize") as number;
+  const globalInsertSpaces = globalEditorSettings.get(
+    "insertSpaces"
+  ) as boolean;
+
+  const extensionSettings = vscode.workspace.getConfiguration(
+    "commit-message-formatter"
+  );
+  const subjectMode = extensionSettings.get(
+    "subjectMode"
+  ) as SubjectFormattingMode;
+  const collapseMultipleEmptyLines = extensionSettings.get(
+    "collapseMultipleEmptyLines"
+  ) as boolean;
+  const protectedPatterns = extensionSettings.get(
+    "protectedPatterns"
+  ) as string[];
+
+  const gitCommitLanguageSettings =
+    vscode.workspace.getConfiguration("[git-commit]");
+  const tabSize =
+    (gitCommitLanguageSettings.get("editor.tabSize") as number) ??
+    (globalTabsize as number);
+  const insertSpaces =
+    gitCommitLanguageSettings.get("editor.insertSpaces") ?? globalInsertSpaces;
+
+  const gitConfig = vscode.workspace.getConfiguration("git");
+  const inputValidationLength = gitConfig.get(
+    "inputValidationLength"
+  ) as number;
+  const inputValidationSubjectLength = gitConfig.get(
+    "inputValidationSubjectLength"
+  ) as number;
+
+  return {
+    lineLength: inputValidationLength,
+    subjectLength: inputValidationSubjectLength,
+    subjectMode,
+    collapseMultipleEmptyLines,
+    tabSize,
+    indentWithTabs: !insertSpaces,
+    protectedPatterns,
+  };
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const config = vscode.workspace.getConfiguration("git");
-  const inputValidationLength = config.get("inputValidationLength") as number;
-  const inputValidationSubjectLength = config.get(
-    "inputValidationSubjectLength"
-  ) as number;
-
-  console.log(config);
-
   vscode.languages.registerDocumentFormattingEditProvider("git-commit", {
     provideDocumentFormattingEdits(
       document: vscode.TextDocument
@@ -24,10 +66,9 @@ export function activate(context: vscode.ExtensionContext) {
         firstLine.range.start,
         lastLine.range.end
       );
-      const formatter = new CommitMessageFormatter({
-        lineLength: inputValidationLength,
-        subjectLength: inputValidationSubjectLength,
-      });
+
+      const config = getConfiguration();
+      const formatter = new CommitMessageFormatter(config);
 
       return [
         vscode.TextEdit.replace(
